1cd4a549975309c2013751354c1c0ed600f4b3c6
JoaoGustavoRogel <joaogustavorogel@hotmail.com>
2020-12-15 19:45:12 -0300

    Create tests for SpotifyCrawler

---
+++ api/tests/test_crawler_spotify.py
@@ -0,0 +1,38 @@
+import os
+import shutil
+import unittest
+
+from datetime import datetime
+from src.models.SpotifyCrawler import ConcreteFactorySpotifyChartsCrawler
+
+
+class TestSpotifyCrawler(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        factory = ConcreteFactorySpotifyChartsCrawler()
+
+        cls.crawler = factory.create_crawler()
+        cls.path = "outputs_spotify"
+
+        try:
+            shutil.rmtree(cls.path)
+        except Exception:
+            pass
+        
+        os.mkdir(cls.path)
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(cls.path)
+
+    def test_get_data(self):
+        extract_data = self.crawler.get_data({
+            "start_date": datetime(2020, 12, 12),
+            "end_date": datetime(2020, 12, 13),
+            "path": self.path,
+        })
+
+        self.assertEqual(len(extract_data), 12347)
+
+if __name__ == "__main__":
+    unittest.main()
