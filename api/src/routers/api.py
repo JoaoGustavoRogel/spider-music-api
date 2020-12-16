192ceb0b60dab3e0aa959d8611a16a9fe1998882
JoaoGustavoRogel <joaogustavorogel@hotmail.com>
2020-12-16 14:46:48 -0300

    Add endpoint for spotify chart extract

--- api/src/routers/api.py
+++ api/src/routers/api.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from src.routers.endpoints import test
+from src.routers.endpoints import spotify_chart
 
 api_router = APIRouter()
 api_router.include_router(test.router, prefix="/test", tags=["test"])
+api_router.include_router(spotify_chart.router, prefix="/spotify")
