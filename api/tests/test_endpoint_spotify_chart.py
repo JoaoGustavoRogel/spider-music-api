bb3a55c1b7123b28bb8ff74ce69bae2faa3d1ac4
dellipi <dellipi@gmail.com>
2020-12-17 21:34:09 -0300

    Add tests to endpoint spotify_chart

---
+++ api/tests/test_endpoint_spotify_chart.py
@@ -0,0 +1,21 @@
+import mock
+import os
+import shutil
+import unittest
+
+from datetime import datetime
+from src.models.SpotifyCrawler import ConcreteFactorySpotifyChartsCrawler, ConcreteSpotifyChartsCrawler
+from src.routers.endpoints.spotify_chart import get_data_chart
+
+
+class TestSpotifyChartsRouter(unittest.TestCase):
+
+    @mock.patch("src.models.SpotifyCrawler.ConcreteSpotifyChartsCrawler")
+    def test_get_data_chart(self, mock_crawler):
+        mock_crawler().get_data.return_value = "testando o endpoint"
+        extract_data = get_data_chart("2020-01-01", "2020-01-10")
+        self.assertEqual(extract_data["data"], "testando o endpoint")
+
+
+if __name__ == "__main__":
+    unittest.main()
