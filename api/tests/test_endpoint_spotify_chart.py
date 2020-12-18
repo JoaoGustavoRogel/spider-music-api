b01500c3f9465e8326460a42a5d4c2ced9c8c486
dellipi <dellipi@gmail.com>
2020-12-17 23:01:32 -0300

    Add mock to mysql_db object

--- api/tests/test_endpoint_spotify_chart.py
+++ api/tests/test_endpoint_spotify_chart.py
@@ -5,17 +5,18 @@ import unittest
 
 from datetime import datetime
 from src.models.SpotifyCrawler import ConcreteFactorySpotifyChartsCrawler, ConcreteSpotifyChartsCrawler
-from src.routers.endpoints.spotify_chart import get_data_chart
+from src.routers.endpoints import spotify_chart
 
 
 class TestSpotifyChartsRouter(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.mock_mysql = mock.patch.object(spotify_chart, "mysql_db", return_value = "instancia_criada")
 
-    @mock.patch("src.models.MySql")
     @mock.patch("src.models.SpotifyCrawler.ConcreteSpotifyChartsCrawler")
-    def test_get_data_chart(self, mock_sql, mock_crawler):
-        mock_sql().instance.return_value = "instancia criada!"
+    def test_get_data_chart(self, mock_crawler):
         mock_crawler().get_data.return_value = "testando o endpoint"
-        extract_data = get_data_chart("2020-01-01", "2020-01-10")
+        extract_data = spotify_chart.get_data_chart("2020-01-01", "2020-01-10")
         self.assertEqual(extract_data["data"], "testando o endpoint")
 
 
