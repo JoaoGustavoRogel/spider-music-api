81d86d50cf47c26aba25106d7ffb2bc07dedf4db
dellipi <dellipi@gmail.com>
2020-12-17 22:28:41 -0300

    Add mock to MySql class

--- api/tests/test_endpoint_spotify_chart.py
+++ api/tests/test_endpoint_spotify_chart.py
@@ -10,8 +10,10 @@ from src.routers.endpoints.spotify_chart import get_data_chart
 
 class TestSpotifyChartsRouter(unittest.TestCase):
 
+    @mock.patch("src.models.MySql")
     @mock.patch("src.models.SpotifyCrawler.ConcreteSpotifyChartsCrawler")
-    def test_get_data_chart(self, mock_crawler):
+    def test_get_data_chart(self, mock_sql, mock_crawler):
+        mock_sql().instance.return_value = "instancia criada!"
         mock_crawler().get_data.return_value = "testando o endpoint"
         extract_data = get_data_chart("2020-01-01", "2020-01-10")
         self.assertEqual(extract_data["data"], "testando o endpoint")
